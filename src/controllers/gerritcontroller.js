7f05d0f30a982d0d3b623ef523dca93306b248f1
Dhaval Maniar <dhaval.maniar@nutanix.com>
2024-05-09 14:48:17 +0530

    Added query parameter to get groupName

--- src/controllers/gerritcontroller.js
+++ src/controllers/gerritcontroller.js
@@ -12,24 +12,25 @@ const agent = new https.Agent({
 });
 
 const test = async (req, res) => {
-    try {
-      const response  = await axios.get(baseURL + '/a/groups/?r=compute-ui&o=MEMBERS', {
-        httpsAgent: agent,
-        auth: {
-          username: username,
-          password: password
-        },
-        transformResponse: [(data) => {
-          return data.substring(data.indexOf('\n') + 1);
-        }]
-      });
-      const parsedData = JSON.parse(response.data);
-      const members = parsedData['compute-ui'].members; 
-      res.status(200).send(members);
-    } catch (error) {
-      console.log(error);
-      res.status(500).send({ message: 'Internal Server Error' });
-    }
+  const groupName = req.query.groupName;
+  try {
+    const response  = await axios.get(baseURL + `/a/groups/?r=${groupName}&o=MEMBERS`, {
+      httpsAgent: agent,
+      auth: {
+        username: username,
+        password: password
+      },
+      transformResponse: [(data) => {
+        return data.substring(data.indexOf('\n') + 1);
+      }]
+    });
+    const parsedData = JSON.parse(response.data);
+    const members = parsedData['compute-ui'].members; 
+    res.status(200).send(members);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
 }
 
 module.exports = { test }
